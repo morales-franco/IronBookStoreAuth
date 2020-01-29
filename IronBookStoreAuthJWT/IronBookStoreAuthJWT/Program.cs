e44810c3124d299054ad4bcb3ddda82a7744a588
Morales Franco <franco.e.morales@gmail.com>
2020-01-29 19:19:02 +1300

    Upload Project - Core Identity

--- IronBookStoreAuthJWT/IronBookStoreAuthJWT/Program.cs
+++ IronBookStoreAuthJWT/IronBookStoreAuthJWT/Program.cs
@@ -14,6 +14,7 @@ namespace IronBookStoreAuthJWT
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
